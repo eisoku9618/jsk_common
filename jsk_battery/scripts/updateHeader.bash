f09ad529c98d3fcd7850cdebc6aea47928b8fa41
eisoku9618 <kuroiwa@jsk.imi.i.u-tokyo.ac.jp>
2015-01-12 00:51:19 +0900

    complete one sensor version

--- jsk_battery/scripts/updateHeader.bash
+++ jsk_battery/scripts/updateHeader.bash
@@ -3,4 +3,4 @@
 rm -rf `rospack find jsk_battery`/sketchbook/libraries/ros_lib
 rosrun rosserial_arduino make_libraries.py `rospack find jsk_battery`/sketchbook/libraries
 
-# sed -i "s#sketchbook.path=.*#sketchbook.path=`rospack find jsk_arduino`/sketchbook#g" ~/.arduino/preferences.txt
+sed -i "s#sketchbook.path=.*#sketchbook.path=`rospack find jsk_battery`/sketchbook#g" ~/.arduino/preferences.txt
--- jsk_battery/sketchbook/battery_remaining_checker/battery_remaining_checker.ino
+++ jsk_battery/sketchbook/battery_remaining_checker/battery_remaining_checker.ino
@@ -1,5 +1,4 @@
 #include <ros.h>
-
 /* srv */
 #include <jsk_battery/BatteryCapacity.h>
 #include <jsk_battery/OutputCharacteristic.h>
@@ -17,9 +16,11 @@ jsk_battery::BatteryStatus bs_msg;
 ros::Publisher pub("battery_state", &bs_msg);
 
 float max_capacity = 15000;
-float used_capacity = 0;
-float ratio = 23;
-float zero_point = 1024 / 2;
+float discharged_capacity = 0;
+int ratio = 23;
+int zero_point = (int)(1024 / 2);
+unsigned long prev_time = 0;
+unsigned long cycle_time = 0;
 
 void bc_cb(const jsk_battery::BatteryCapacity::Request &req, jsk_battery::BatteryCapacity::Response &res) {
   max_capacity = req.battery_capacity;
@@ -28,8 +29,11 @@ void rovic_cb(const jsk_battery::OutputCharacteristic::Request &req, jsk_battery
   ratio = req.ratio;
 }
 void zpa_cb(const jsk_battery::ZeroPointAdjustment::Request &req, jsk_battery::ZeroPointAdjustment::Response &res) {
-  zero_point = req.zero_point;
-  /* zero_point = averageAnalog(0, 10); */
+  char buf[15];
+  zero_point = averageAnalog(PIN, 10);
+  dtostrf(zero_point, 5, 2, buf);
+  nh.loginfo("set zero point:");
+  nh.loginfo(buf);
 }
 
 ros::ServiceServer<jsk_battery::BatteryCapacity::Request, jsk_battery::BatteryCapacity::Response> capacity_service("set_battery_capacity", &bc_cb);
@@ -37,16 +41,12 @@ ros::ServiceServer<jsk_battery::OutputCharacteristic::Request, jsk_battery::Outp
 ros::ServiceServer<jsk_battery::ZeroPointAdjustment::Request, jsk_battery::ZeroPointAdjustment::Response> calib_service("set_zero_point_adjustment", &zpa_cb);
 
 
-uint32_t prev_time = 0;
-uint32_t cur_time = 0;
-uint32_t cycle_time = 0;
-
-float averageAnalog(int pin, int num){
-  float v = 0;
+int averageAnalog(int pin, int num){
+  int v = 0;
   for(int i=0; i<num; i++) {
     v+= analogRead(pin);
   }
-  return v / num;
+  return (int)(v / num);
 }
 
 void setup() {
@@ -59,56 +59,40 @@ void setup() {
   nh.advertise(pub);
   /* initialize */
   zero_point = averageAnalog(PIN, 20);
+  delay(50);
   prev_time = micros();
 }
 
 void main_loop() {
-  float value = averageAnalog(PIN, 4);    /* [bit] */
-  char buf[15];
-  /* 
-   * dtostrf(value, 5, 2, buf);
-   * nh.loginfo(buf);
-   * dtostrf(zero_point, 5, 2, buf);
-   * nh.loginfo(buf);
-   */
-
-  float current;
-  float remaining_capacity;
-  float remaining_percentage;
-
-  current = (value - zero_point) * (VREF * 1e3 / 1024) / ratio; /* [A] = [bit] * [mV/bit] / [mV/A] */
-  used_capacity += current * ((float)cycle_time * 1e-6); /* [As] = [A] * [s] */
-  remaining_capacity = max_capacity - used_capacity * 1e3 / 60 / 60; /* [mAh] */
-  remaining_percentage = remaining_capacity / max_capacity * 100;
+  int value = averageAnalog(PIN, 4); /* [bit] */
+  float current = (value - zero_point) * (VREF * 1e3 / 1024) / ratio; /* [A] = [bit] * [mV/bit] / [mV/A] */
+  unsigned long cur_time = micros();
+  cycle_time = cur_time - prev_time;
+  if (cycle_time < 0) {
+    cycle_time += pow(2, sizeof(unsigned long)*8);
+  }
+  prev_time = cur_time;
+  discharged_capacity += current * (float)cycle_time * 1e-6; /* [As] = [A] * [s] */
+  float remaining_capacity = max_capacity - discharged_capacity * 1e3 / 60 / 60; /* [mAh] */
+  float remaining_percentage = remaining_capacity / max_capacity * 100;
 
   bs_msg.current = current;
   bs_msg.remaining_capacity = remaining_capacity;
   bs_msg.remaining_percentage = remaining_percentage;
-
-  /* 
-   * bs_msg.current = (value - zero_point) * (5.0 / 1024) / ratio; /\* current [mA] : [bit] * [V/bit] / [mV/A] *\/
-   * used_capacity += bs_msg.current * (cycle_time * 1e-6) / 60 / 60;        /\* [mA] * [h] *\/
-   * bs_msg.remaining_capacity = (max_capacity - used_capacity);
-   * bs_msg.remaining_percentage = (max_capacity - used_capacity) / max_capacity * 100;
-   */
-  delay(50);
-
-  cur_time = micros();
-  cycle_time = cur_time - prev_time;
-  prev_time = cur_time;
+  bs_msg.analog_value = value;
+  bs_msg.zero_point = zero_point;
+  bs_msg.discharged_capacity = discharged_capacity;
+  bs_msg.cycle_time = (float)cycle_time;
+  bs_msg.diff = (current * ((float)cycle_time * 1e-6));
 }
 
 void loop() {
-  for(int i=0; i<10; i++) {
+  for(int i=0; i<20; i++) {
+    delay(50);                  /* need delay for analog read */
     main_loop();
   }
-  /* 
-   * char buf[15];
-   * dtostrf(cycle_time, 5, 2, buf);
-   * nh.loginfo(buf);
-   * dtostrf(used_capacity, 10, 2, buf);
-   * nh.loginfo(buf);
-   */
+
+  /* nh.loginfo(String(sizeof(unsigned int))); */
   pub.publish(&bs_msg);
   nh.spinOnce();
 }
